9daeed49d1bda11eceec93083164d9988795b192
Your NameKarem <hamze.karim99@gmail.com>
2022-04-13 22:17:34 +0300

    homework

--- validationDetails/js/script.js
+++ validationDetails/js/script.js
@@ -1,37 +1,31 @@
 function validationIsraelID() {
-    let ID = [], sum = 0, digit = 0;
-    ID[0] = document.getElementById("digit1").value;
-    ID[1] = document.getElementById("digit2").value;
-    ID[2] = document.getElementById("digit3").value;
-    ID[3] = document.getElementById("digit4").value;
-    ID[4] = document.getElementById("digit5").value;
-    ID[5] = document.getElementById("digit6").value;
-    ID[6] = document.getElementById("digit7").value;
-    ID[7] = document.getElementById("digit8").value;
-    ID[8] = document.getElementById("digit9").value;
+    let ID, sum = 0, digit = 0;
+    ID = document.getElementById("digit1");
 
-    for (let i = 0; i < ID.length - 1; i++) {
-        if(!ID[i]>=0 && !ID[i]<=10){
-            console.log("enter your ID again !!!!!");
-        }
+    if (ID.value.length > 9) {
+        alert("error id");
+        return;
+    }
+    for (let i = 0; i < ID.value.length - 1; i++) {
+
         if (i % 2 == 0) {
-            digit = ID[i] * 1;
+            digit = ID.value[i] * 1;
         }
         else {
-            digit = ID[i] * 2;
+            digit = ID.value[i] * 2;
         }
         if (digit >= 10) {
             digit = (digit % 10) + Math.floor(digit / 10);
         }
         sum += digit;
     }
-    if ((sum + ID[8] * 1) % 10 == 0) {
+    if ((sum + ID.value[8] * 1) % 10 == 0) {
         console.log("true");
     }
     else {
         console.log("false");
     }
 
-
+    ID.value = "";
 
 }
